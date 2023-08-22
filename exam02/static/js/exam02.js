4bbd92fc8ee3a4a258fa0e7ecfc27483ab12a3ce
JisooOvO <fdking1887@naver.com>
2023-08-22 13:09:22 +0900

    add exam02

---
+++ exam02/static/js/exam02.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("ah1").innerHTML = "Hello JavaScript!";
+} );
+
+//alert
+
+function hello(){
+    console.log("Hello JS");
+}
